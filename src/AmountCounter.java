5ebff4b57cf3f6312843a5fc72497908bd8c4310
imLIVI <lesly120898@gmail.com>
2022-11-22 01:15:37 +0300

    Implement a multithreaded method for calculaing sums

--- src/AmountCounter.java
+++ src/AmountCounter.java
@@ -1,2 +1,22 @@
-public class AmountCounter extends {
+import java.util.Arrays;
+import java.util.concurrent.RecursiveTask;
+
+public class AmountCounter extends RecursiveTask<Integer> {
+    private int[] array;
+
+    public AmountCounter(int[] array) {
+        this.array = array;
+    }
+
+    @Override
+    protected Integer compute() {
+        if (array.length <= 2)
+            return Arrays.stream(array).sum();
+
+        AmountCounter left = new AmountCounter(Arrays.copyOfRange(array, 0, array.length / 2));
+        AmountCounter right = new AmountCounter(Arrays.copyOfRange(array, array.length / 2, array.length));
+        left.fork();
+        right.fork();
+        return left.join() + right.join();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,55 @@
 import java.util.Arrays;
 import java.util.Random;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
-    public static final int ARRAY_SIZE = 10_000_000;
-    public static final int MIN_ARRAY_SIZE = 10_000;
+    public static final int[] ARRAYS_SIZE = {10_000, 10_000_000};
     public static final String STRING_FORMAT = "%-30s %-10s %-10s\n";
     public static final String SINGLE_THREAD_STRING = "Single-threaded counting";
     public static final String MULTI_THREAD_STRING = "Multithreaded counting";
 
 
     public static void main(String[] args) {
-        int[] array = initArray();
+        for (int arraySize : ARRAYS_SIZE)
+        {
+            int[] array = initArray(arraySize);
+            System.out.printf("\nArray`s size = %s\n---------------------------------------------------\n",
+                    arraySize);
 
-        System.out.printf(STRING_FORMAT, "", "Time, ns", "Amount");
-        long startTime = System.nanoTime();
-        int amount = sumBySingle(array);
-        long endTime = System.nanoTime();
-        System.out.printf(STRING_FORMAT, SINGLE_THREAD_STRING, endTime - startTime, amount);
-        //startTime = System.currentTimeMillis();
-        //methodToTime();
-        //endTime = System.currentTimeMillis();
-        //System.out.printf(MULTI_THREAD_STRING, endTime - startTime);
+            // 1 method - Single threaded
+            System.out.printf(STRING_FORMAT, "", "Time, ns", "Amount");
+            long startTime = System.nanoTime();
+            int amount = sumBySingle(array);
+            long endTime = System.nanoTime();
+            System.out.printf(STRING_FORMAT, SINGLE_THREAD_STRING, endTime - startTime, amount);
+
+            // 2 method - Multithreaded
+            AmountCounter amountCounter = new AmountCounter(array);
+            // limiting the number of threads
+            int numOfThreads = Runtime.getRuntime().availableProcessors();
+            ForkJoinPool forkJoinPool = new ForkJoinPool(numOfThreads);
+
+            startTime = System.currentTimeMillis();
+            amount = forkJoinPool.invoke(amountCounter);
+            endTime = System.currentTimeMillis();
+            System.out.printf(STRING_FORMAT, MULTI_THREAD_STRING, endTime - startTime, amount);
+        }
     }
 
-    public static int[] initArray() {
-        int[] array = new int[ARRAY_SIZE];
+    public static int[] initArray(int arraySize) {
+        int[] array = new int[arraySize];
         Random random = new Random();
-        for (int i = 0; i < ARRAY_SIZE; i++) {
-            array[i] = random.nextInt(ARRAY_SIZE) + 1;
+        for (int i = 0; i < arraySize; i++) {
+            array[i] = random.nextInt(arraySize) + 1;
         }
         return array;
     }
 
     public static int sumBySingle(int[] array) {
         int sum = 0;
-        for (int i = 0; i < ARRAY_SIZE; i++) {
+        for (int i = 0; i < array.length; i++) {
             sum += array[i];
         }
         return sum;
     }
-
 }
